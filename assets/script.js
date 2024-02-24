bd8444864404d151ad5aa7c2cb0a5dcc91cf923e
Thayna Bittencourt Baima <131307382+thaynahakan@users.noreply.github.com>
2024-02-24 18:01:01 -0300

    tentando inserir um botão de edição

--- assets/script.js
+++ assets/script.js
@@ -45,16 +45,26 @@ function newTask() {
    Exibe as tarefas na lista, incluindo um botão para remover cada tarefa.
    Utiliza um loop para percorrer o array de tarefas e gera o HTML correspondente.
 */
+
+
 function showValues() {
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
     let list = document.getElementById('to-do-list');
     list.innerHTML = '';
 
     for (let i = 0; i < values.length; i++) {
-        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-                            <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
-                            </svg></button></li>`;
+        list.innerHTML += `<li>${values[i]['name']}
+                            <button id='btn-edit' onclick='editItem("${values[i]['name']}")'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                    <path d="M12.293 2.293a1 1 0 0 1 1.414 0l1 1a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-1.414 0l-1-1a1 1 0 0 1 0-1.414l9-9zM14 6l-2-2 1-1 2 2-1 1zm-3.793 8.707a1 1 0 0 0-.126.127l-2 2a1 1 0 0 0 1.414 1.414l2-2a1 1 0 0 0-.288-1.41 1 1 0 0 0-1.414 0zM2 13h10v2H2z"/>
+                                </svg>
+                            </button>
+                            <button id='btn-ok' onclick='removeItem("${values[i]['name']}")'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                    <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+                                </svg>
+                            </button>
+                        </li>`;
     }
 }
 
